9d1589e3ec4dc7376cb909f9917d2333d42002ed
guites <gui.garcia67@gmail.com>
2022-08-31 14:22:40 -0300

    Add command to clock in

--- nhow.sh
+++ nhow.sh
@@ -1,16 +1,31 @@
-minha_matricula="1234"
-minha_senha="minhasenha"
-codigo_empresa="abc123"
+
+# reads values from config file
+# $1 is variable name in .env
+read_var_config()
+{
+    config_file=.env
+    sed -nr 's/^'$1'=(.*)$/\1/p' $config_file
+}
+
+# set global configuration variables
+identity=$(read_var_config identity)
+codigo_empresa=$(read_var_config codigo_empresa)
+numero_matricula=$(read_var_config numero_matricula)
+senha=$(read_var_config senha)
+urlencoded_senha=$(bash urlencode.sh "$senha")
+long=$(read_var_config longitude)
+lat=$(read_var_config latitude)
 
 Help() {
    # Display Help
-   echo "Este script vai te mostrar o horário no qual você bateu os pontos hoje."
+   echo "Este script vai te mostrar os horários nos quais você bateu o ponto hoje."
    echo
-   echo "sintaxe: nhow.sh [-j|p|h]"
+   echo "sintaxe: nhow.sh [-j|e|p|h]"
    echo "opções:"
-   echo "j     Gera um novo jwt para autenticação no sistema."
-   echo "p     Imprime os horários nos quais você bateu o ponto hoje."
-   echo "h     Imprime este diálogo de ajuda."
+   echo "j     (jwt) Gera um novo jwt para autenticação no sistema."
+   echo "e     (espelho) Imprime os horários nos quais você bateu o ponto hoje."
+   echo "p     (ponto) Bate o ponto no horário atual."
+   echo "h     (help) Imprime este diálogo de ajuda."
    echo
 }
 
@@ -30,36 +45,53 @@ get_jwt() {
         -H 'sec-fetch-dest: empty' \
         -H 'sec-fetch-mode: cors' \
         -H 'sec-fetch-site: same-origin' \
-        --data-raw "empresa=$codigo_empresa&origin=portal&matricula=$minha_matricula&senha=$minha_senha" \
+        --data-raw "empresa=$codigo_empresa&origin=portal&matricula=$numero_matricula&senha=$urlencoded_senha" \
         --compressed)
-
     my_jwt=$(echo $login_response | jq .jwt | sed 's/"//g')
     echo $my_jwt > .nhow.jwt
 }
 
 get_todays_ponto() {
     my_jwt=$(cat .nhow.jwt 2>/dev/null)
-    [ $? == 1 ] && echo "You need to generate a new jwt. Run <./nhow -j>" && exit 1
+    [ $? == 1 ] && echo "You need to generate a new jwt. Run <./nhow.sh -j>" && exit 1
     today=$(date -I)
     curl https://www.nhow.com.br/api-espelho/apuracao/ -H "Authorization: Bearer $my_jwt" | jq ".dias[] | select(.referencia==\"$today\")".batidas
 }
 
+hit_ponto() {
+    timestamp=$(date +%s%N | cut -b1-13)
+    echo "Bater ponto? [s|N]"
+    read should_proceed
+    [[ $should_proceed != 's' ]] && echo "Finalizando programa sem bater o ponto..." && exit 0
+
+    curl 'https://www.nhow.com.br/batidaonline/verifyIdentification' \
+      -H 'authority: www.nhow.com.br' \
+      -H 'accept: application/json;charset=UTF-8' \
+      -H 'cache-control: no-cache' \
+      -H 'content-type: text/plain;charset=UTF-8' \
+      --data-raw '{"identity":"'"$identity"'","account":"'"$numero_matricula"'","password":"'"$senha"'","login":false,"offline":true,"timestamp":'"$timestamp"',"origin":"chr","version":"1.0.25","identification_type":"matricula_senha","longitude":'"$long"',"latitude":'"$lat"',"accuracy":14.813,"provider":"network/wifi"}' \
+      --compressed
+}
+
 # handle script options
-while getopts ":jp" option; do
-   case $option in
-      j) # get a new jwt and save to file
-        get_jwt
-        exit;;
-      p) # print daily pontos
-        get_todays_ponto
-        exit;;
-      h) # print help
-        Help
-        exit;;
-      \?) # invalid option
-        echo "Invalid option"
-        Help
-        exit;;
+while getopts ":jeph" option; do
+    case $option in
+        j) # get a new jwt and save to file
+            get_jwt
+            exit;;
+        e) # print daily pontos
+            get_todays_ponto
+            exit;;
+        p) # hits ponto with current timestamp
+            hit_ponto
+            exit;;
+        h) # print help
+            Help
+            exit;;
+        \?) # invalid option
+            echo "Invalid option"
+            Help
+            exit;;
    esac
 done
 
